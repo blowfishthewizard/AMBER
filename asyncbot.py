3cf9ad34046fe0f06558ceff02442bd832090fb7
ISOSCELES <62076465+blowfishthewizard@users.noreply.github.com>
2020-05-18 19:00:38 -0400

    edited FUNCTION

--- asyncbot.py
+++ asyncbot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import asyncio
 import ssl
+import time
 from datetime import datetime
 import config
 
@@ -49,54 +50,9 @@ class IRC:
 					elif args[1] == '001':
 						self._raw('JOIN ' + config.connection.channel)
 					elif 'KUSH' in line:
-						_missingchatter()
-					elif 'test' in line: # check for test msg in channel
-						self._raw('NAMES ' + config.connection.channel0) #send /NAMES query to channel
-					elif len(args) >= 4 and args[3] == '=': #if response is long enough to contain names and is direct reply to bot from server
-						print("dasit mane ") #we can read the list of users for channel
-						#print(args[5:]) #this is the /NAMES response from server as an array with everything removed except nicks
-						chan_nicklist = args[5:] #put our list into its own var
-						#so now we can run thru names and do whois and our data manipulation etc
-						for i in chan_nicklist: 
-							self._raw('whois ' + i.replace('@','').replace('+',''))
-					elif len(args) == 10 and args[9] == 'time':
-						print("checking")
-						print(args)
-						print(args[3] + " idle for " + args[4] + " seconds")
-						daysmissing = str(round(float(args[4]) / 86400, 2))
-						if int(args[4]) >= int(172800):#2700
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '[!] MISSINGCHATTER: ' + args[3] + " lastseen " + args[4] + " seconds.")
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,4                                                  ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,4  1,8^^^^^^1,4  1,1   1,4 1,1     1,4 1,1  1,4  1,1  1,4 1,1  1,4   1,1   1,4 1,1 1,4  1,1  1,4 1,1  1,4  1,1   1,4 ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,4 1,8<0,2 **** 1,8>1,4 1,1 1,4 1,1 1,4 1,1 1,4 1,1 1,4 1,1 1,4 1,1 1,4 1,1 1,4 1,1 1,4  1,1 1,4 1,1 1,4  1,1 1,4 1,1 1,4 1,1 1,4  1,1 1,4  1,1 1,4 1,1 1,4  1,1 1,4  ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,4 1,8<0,2*CFLC*1,8>1,4 1,1   1,4 1,1 1,4 1,1 1,4 1,1 1,4 1,1  1,4  1,1  1,4 1,1  1,4   1,1   1,4 1,1 1,4  1,1  1,4 1,1  1,4   1,1 1,4  ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,4 1,8<0,2 **** 1,8>1,4 1,1 1,4 1,1 1,4 1,1 1,4   1,1 1,4 1,1 1,4 1,1 1,4 1,1 1,4  1,1 1,4 1,1 1,4  1,1 1,4 1,1 1,4 1,1 1,4  1,1 1,4  1,1 1,4 1,1 1,4  1,1 1,4  ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,4  1,8VVVVVV1,4  1,1 1,4 1,1 1,4 1,1 1,4   1,1 1,4 1,1  1,4  1,1  1,4 1,1 1,4 1,1 1,4  1,1 1,4 1,1 1,4 1,1  1,4 1,1  1,4 1,1 1,4 1,1 1,4  1,1 1,4  ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,4                                                  ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,1                                                  ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0                                                  ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0                                                  ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1                    1,0                            ')
-							self._raw(f'PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,10                  1,1 1,0   12NAME   1: ' + args[3] + '            ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,10 5,7,;\',;\',,5,10 1        1,1 1,0                            ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,10 5,7.;\'.  ( _5,10 1       1,1 1,0  12 AGE    1: 16 AND HALF     ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,10 5,7.1@5;;1  0O O 1,10       1,1 1,0                            ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,10 5,7.1 5; 1    > 1,10       1,1 1,0  12 HEIGHT1 : 5\' 12"          ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,10 5,7;1    5 ;;;;5,10  1     1,1 1,0                            ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,10 1,7      1,1___1,10 1,6\  1,10    1,1 1,0  12 WEIGHT1 : 330LBS 14(FNO) 1   ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,7          1,10 1,6  1,7   1,10  1,1 1,0                            ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1 1,7     1,10         1,7   1,10 1,1 1,0   12EYES  1 : BROWN           ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1,1                    1,0                            ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0                                                  ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  Missing from #superbowl, SuperNETs for   ' + daysmissing + ' days ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0                                                  ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  ANY INFORMATION REGARDING THE WHERE-ABOUTS OF   ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  THIS CHATTER SHOULD REPORT IT TO THE OFFICAL    ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  CENTER FOR LOST CHATTERS 14(CFLC)1 AS SOON AS OK.  ')
-#							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0                                                  ')
-							self._raw('PRIVMSG ' + config.connection.channel + " :" + '1,0  1-800-5MISSING1                 missing@cflc.gov  ')
+						self.missingchatter()
 
-	async def _missingchatter(self, interval):
+	async def missingchatter(self, interval):
 		while True:
 			await asyncio.sleep(interval)
 			self._raw('NAMES ' + config.connection.channel)
@@ -133,6 +89,6 @@ class IRC:
 if __name__ == '__main__':
 	Bot = IRC()
 	asyncio.run(Bot._connect())
-	asyncio.run(Bot._missingchatter(60*1))
+	asyncio.run(Bot.self.missingchatter(1*1))
 	while True: # Keep-alive loop, since we are asyncronous
 		input('')
